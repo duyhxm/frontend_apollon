1d2b12a74479d423875233570e361bdef4afbfb4
duyhxm <2251012049duy@ou.edu.vn>
2023-12-31 01:10:23 +0700

    1:10 am 12.31.2023

--- Src/script.js
+++ Src/script.js
@@ -3,18 +3,45 @@ const dbgr = document.querySelector("#debugger");
 const specifications = header.getBoundingClientRect();
 const x = specifications.x;
 const y = specifications.y;
-const rightHeader = specifications.right;
+const leftHeader = specifications.left;
+const heightHeader = Math.ceil(specifications.height).toFixed(2);
 const viewPortWidth = window.innerWidth;
+const publicLetter = document.querySelector('#public_letter');
+const specPublicLetter = publicLetter.getBoundingClientRect();
+const yPublicLetter = specPublicLetter.top + window.scrollY;
+const intro = document.querySelector("#introduction");
+const letterSection = document.querySelector("#letter_section");
+const aboutUs = document.querySelector("#about_us");
+const arr = document.querySelectorAll('section');
+const main = document.querySelector('main');
+const t = Math.ceil(intro.getBoundingClientRect().height + letterSection.getBoundingClientRect().height + aboutUs.getBoundingClientRect().height).toFixed(2);
+const letterContainer = document.querySelector(".content__container--template");
+const heightLetterContainer = letterContainer.offsetHeight;
 
-console.log(rightHeader);
 
-document.documentElement.style.setProperty('--coordinates_nav_table', `${viewPortWidth - rightHeader}px`);
+const g = 3.472222222*3/100*viewPortWidth + heightLetterContainer;
+const heightPublicLetter = parseFloat(publicLetter.offsetHeight);
+
+let k = Math.ceil(g).toFixed(2);
+let j = intro.offsetHeight + aboutUs.offsetHeight + parseFloat(k) - parseFloat(heightHeader);
+document.documentElement.style.setProperty('--h-content-container', `${Math.ceil(heightLetterContainer).toFixed(2)}px`);
+document.documentElement.style.setProperty('--coordinates_nav_table', `${leftHeader}px`);
 window.addEventListener('scroll', () =>{
-    let a = specifications.top + document.documentElement.scrollTop;
-    dbgr.innerHTML = `${document.documentElement.scrollLeft} <br> ${Math.ceil(a).toFixed(2)}`;
+    let a = Math.ceil(specifications.top + document.documentElement.scrollTop).toFixed(2);
+    dbgr.innerHTML = `${document.documentElement.scrollLeft} <br> ${a} <br> ${t} <br> ${yPublicLetter} <br> ${g} <br> ${viewPortWidth} <br> ${j} <br> ${publicLetter.offsetHeight}`;
+    if(parseFloat(a) >= j && parseFloat(a) <= j + heightPublicLetter)
+    {
+        header.classList.add("change");
+    }
+    else{
+        header.classList.remove("change");
+    }
 })
 const iconHamburger = document.querySelector("#button__hamburger");
 const navTable = document.querySelector('#nav_table');
+const templateText = document.querySelector('#template_text');
+const widthTemplateText = templateText.offsetWidth;
+document.documentElement.style.setProperty('--w-content-nav-table', `${Math.ceil(widthTemplateText)}px`);
 
 iconHamburger.addEventListener('click', () => {
     if(iconHamburger.classList.contains('click')){
@@ -25,14 +52,4 @@ iconHamburger.addEventListener('click', () => {
         iconHamburger.classList.add('click');
         navTable.classList.add('opened');
     }
-})
-
-// if(viewPortWidth >= 1440 && viewPortWidth < 2559){
-//     document.documentElement.style.setProperty("--w-button-letter", '130px');
-//     document.documentElement.style.setProperty("--h-button-letter", '56px');
-//     document.documentElement.style.setProperty("--s-icon-pencil", '24px');
-//     document.documentElement.style.setProperty("--fs-button-letter", '20px');
-//     document.documentElement.style.setProperty("--g", '8px');
-//     document.documentElement.style.setProperty("--b", '15px');
-      
-// }
+});
